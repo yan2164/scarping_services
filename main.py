41826f0cb79baff6b7156605eb589f0310158ce7
yan2164 <yanuariusbasilius@gmail.com>
2021-11-04 01:36:05 +0700

    Create API service

---
+++ main.py
@@ -0,0 +1,83 @@
+from fastapi import FastAPI
+from pydantic import Field
+from pydantic.errors import UrlError
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+app = FastAPI()
+
+def getAmazonItem(url):
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.minimize_window()
+    driver.get(url)
+    try:
+        items = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.ID, "ppd"))
+        )
+        title = items.find_element_by_xpath('//*[@id="productTitle"]').text
+        price_id = ['//*[@id="priceblock_ourprice"]', '//*[@id="corePrice_desktop"]/div/table/tbody/tr/td[2]/span[1]/span[2]', '//*[@id="actualPriceValue"]/strong']
+        for id in price_id:
+            try:
+                price = items.find_element_by_xpath(id).text
+            except:
+                pass
+        img = items.find_element_by_xpath('//*[@id="landingImage"]').get_attribute('src')
+        item_list={
+            'title' : title,
+            'price' : price,
+            'img' : img,
+        }
+        return(item_list)
+    finally:
+        driver.quit()
+
+
+def getEbayItem(url):
+    s = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=s)
+    driver.minimize_window()
+    driver.get(url)
+    try:
+        items = WebDriverWait(driver, 2).until(
+            EC.presence_of_element_located((By.ID, "BottomPanelDF"))
+        )
+        title = items.find_element_by_xpath('//*[@id="itemTitle"]').text
+        price_id = ['//*[@id="prcIsum"]', '//*[@id="prcIsum_bidPrice"]', '//*[@id="mm-saleDscPrc"]']
+        for id in price_id:
+            try:
+                price = items.find_element_by_xpath(id).text
+            except:
+                pass
+        img = items.find_element_by_xpath('//*[@id="icImg"]').get_attribute('src')
+        item_list={
+            'title' : title,
+            'price' : price,
+            'img' : img,
+        }
+        return(item_list)
+    finally:
+        driver.quit()
+
+class Scrape():
+    url: str = Field(...,lt=200, description="Paste Amazon or Ebay item's link here")
+
+@app.get("/api/get_data/")
+def page(url):
+    try:
+        if 'www.amazon' in url:
+            get_data = getAmazonItem(url)
+            return get_data
+        elif 'www.ebay' in url:
+            get_data = getEbayItem(url)
+            return get_data
+        else:
+            return "url not valid"
+    except UrlError as e:
+        print(e)
+        return e
+    
